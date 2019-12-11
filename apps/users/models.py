f92738bc4e68871766a391c150b805b7b580281f
Dy0mar <kurtudins@gmail.com>
2019-12-11 20:42:26 +0200

    email must be required

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 
 class User(AbstractUser):
+    email = models.EmailField(_('email address'))
 
     def __str__(self):
         return "{}".format(self.email)
