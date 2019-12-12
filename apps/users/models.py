15e98f07992e446603ccd29e76784acab9c64c27
Dy0mar <kurtudins@gmail.com>
2019-12-12 15:01:25 +0200

    fix username and email

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 
 class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            'Required. 150 characters or fewer. '
+            'Letters, digits and @/./+/-/_ only.'
+        ),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+        null=True
+    )
+
+    email = models.EmailField(_('email address'), blank=False)
 
     def __str__(self):
         return "{}".format(self.email)
