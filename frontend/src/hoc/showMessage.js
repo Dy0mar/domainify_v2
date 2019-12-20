cb7f2dbe68bcfd82f636d39a9deaf47630e0ba4f
Dy0mar <kurtudins@gmail.com>
2019-12-20 12:43:01 +0200

    get last message from user-selector

--- frontend/src/hoc/showMessage.js
+++ frontend/src/hoc/showMessage.js
@@ -3,15 +3,16 @@ import 'antd/dist/antd.css';
 import { message } from 'antd';
 import {connect} from "react-redux";
 import {showedMessage} from "../redux/app-reducer";
+import {getLastMessage} from "../redux/users-selectors";
 
 let mapStateToProps = (state) => ({
-    messages: state.app.messages,
+    lastMessage: getLastMessage(state)
 });
 
 export const showMessage = (Component) => {
     class showMessageComponent extends React.Component {
         show = () => {
-            const messObj = this.props.messages[this.props.messages.length-1];
+            const messObj = this.props.lastMessage;
             if (messObj && messObj.id > 0){
                 message[messObj.type](messObj.message);
                 this.props.showedMessage(messObj)
