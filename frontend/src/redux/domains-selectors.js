5e0afb489cd3845a3818d0dff9925795a917d4f6
Dy0mar <kurtudins@gmail.com>
2019-12-25 18:43:13 +0200

    add selector for filter managers

--- frontend/src/redux/domains-selectors.js
+++ frontend/src/redux/domains-selectors.js
@@ -2,6 +2,10 @@
 
 export const getDomainListS = (state) => state.domains.results;
 
+export const getManagersListS = (state) => (
+    state.domains.managers.map(u => ({text: u, value: u,}))
+);
+
 export const getDomainListPageTotalS = (state) => state.domains.count;
 
 export const getUrlOr404S = (url) => {
