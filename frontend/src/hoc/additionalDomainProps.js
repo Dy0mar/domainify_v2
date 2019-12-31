524ed2a1eb9fc6e9c30c2fbdddf2f5803cb90758
Dy0mar <kurtudins@gmail.com>
2019-12-31 17:49:51 +0200

    add hoc additionalDomainProps

---
+++ frontend/src/hoc/additionalDomainProps.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {connect} from "react-redux";
+import {getManagerListS} from "../redux/users-selectors";
+import {
+    getAlexaStatusListS,
+    getCompanyListS,
+    getDomainStatusListS
+} from "../redux/domains-selectors";
+
+let mapStateToProps = (state) => ({
+    formErrors: state.domains.formErrors,
+    managers: getManagerListS(state),
+    statuses: getDomainStatusListS(state),
+    companies: getCompanyListS(state),
+    alexa_statuses: getAlexaStatusListS(state),
+});
+
+export const additionalDomainProps = (Component) => {
+    const wrapper = (props) => {
+
+        return <Component {...props}/>
+    };
+
+    let wrapperComponent = connect(mapStateToProps)(wrapper);
+
+    return wrapperComponent
+};
