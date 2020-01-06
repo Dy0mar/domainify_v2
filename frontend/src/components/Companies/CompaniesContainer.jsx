b3e14405fa63fa356ef87fd9028cddffa57417c5
Dy0mar <kurtudins@gmail.com>
2020-01-06 17:53:20 +0200

    add company list

--- frontend/src/components/Companies/CompaniesContainer.jsx
+++ frontend/src/components/Companies/CompaniesContainer.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
-import {Divider, Row, Col} from 'antd';
+import {Divider, Row, Col, Table} from 'antd';
 import "antd/dist/antd.css";
 import {compose} from "redux";
 import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
+import {NavLink, withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {getCompanyListS} from "../../redux/company-selector";
+import {getUrlOr404S} from "../../redux/domains-selectors";
 
 
 const CompaniesContainer = (props) => {
+    const { companies } = props;
+
+    const config = {
+        dataSource: companies,
+        rowKey: item => item.pk,
+        columns: [
+            {
+                title: 'Company name',
+                dataIndex: 'name',
+                key: 'name',
+                render: (name, row) => <NavLink to={getUrlOr404S(row.url)} >{name}</NavLink>
+            },
+            {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
+            },
+        ],
+    };
 
     return (
         <div>
             <Divider>Companies here</Divider>
             <Row>
                 <Col span={24}>
-                    table
+                    <Table {...config} />
                 </Col>
             </Row>
         </div>
@@ -22,7 +43,7 @@ const CompaniesContainer = (props) => {
 };
 
 const mapStateToProps = (state) => ({
-    user: state.user,
+    companies: getCompanyListS(state),
 });
 
 export default compose(
