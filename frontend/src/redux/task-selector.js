dd0f47652a489424966e79b3fb4c770e19524cf2
Dy0mar <kurtudins@gmail.com>
2020-01-14 17:28:57 +0200

    add selector for filters

--- frontend/src/redux/task-selector.js
+++ frontend/src/redux/task-selector.js
@@ -1,6 +1,12 @@
 export const getTaskListS = state => state.tasks.results;
 export const getCodeListS = state => state.tasks.codes;
-export const getStatusListS = state => state.tasks.statuses;
+export const getStatusListS = state => {
+    return state.tasks.statuses.map(item => ({
+        ...item,
+        value: item.pk,
+        text: item.status,
+    }));
+};
 
 export const getStatusByIdS = (state, statusId) => getStatusListS(state).filter(item => item.pk === parseInt(statusId))[0];
 
