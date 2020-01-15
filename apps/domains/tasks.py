744191082e5076f992c7bfb3f45144685c7fea0c
Dy0mar <kurtudins@gmail.com>
2020-01-15 18:20:32 +0200

    install python-whois and tasks for domains

---
+++ apps/domains/tasks.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import time
+import datetime
+
+from django.db.models import Q
+from whois import whois
+from celery import shared_task
+
+from domains.models import Domain
+
+
+@shared_task
+def auto_update_whois():
+    now = datetime.datetime.now().date()
+    expire_period = (
+        now + datetime.timedelta(days=30)
+    )
+
+    qs = Domain.objects.filter(
+        Q(register_date__isnull=True) |
+        Q(expire_date__isnull=True) |
+        Q(
+            Q(expire_date__gte=now),
+            Q(expire_date__lte=expire_period)
+        )
+    ).exclude(status=Domain.CLOSED)
+
+    for domain in qs:
+        try:
+            w = whois(domain.name)
+        except Exception:
+            continue
+
+        domain.date_register = w.creation_date.date()
+        domain.date_expire = w.expiration_date.date()
+        domain.save()
+        time.sleep(2)
+
+
+@shared_task
+def auto_close_domain():
+    qs = Domain.objects.filter(
+        date_expire__lt=datetime.datetime.now(),
+    ).exclude(status=Domain.CLOSED)
+
+    for domain in qs:
+        domain.status = Domain.CLOSED
+        domain.save()
+        time.sleep(1)
+
