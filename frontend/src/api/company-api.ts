1719762578d4dcbc6a4313db5b7c2487cf59a6a8
Dy0mar <kurtudins@gmail.com>
2020-06-15 17:15:29 +0300

    fix companies

--- frontend/src/api/company-api.ts
+++ frontend/src/api/company-api.ts
@@ -3,15 +3,15 @@ import {TCompany} from "../types/g-types";
 
 export const companyAPI = {
     company_list() {
-        return instance.get<TCompany>('companies/').then(r => r.data)
+        return instance.get<Array<TCompany>>('companies').then(r => r.data)
     },
     create(data: any) {
-        return instance.post('companies/', {...data}).then(r => r.data)
+        return instance.post('companies', {...data}).then(r => r.data)
     },
     patch_field(pk: number, data: any) {
-        return instance.patch(`companies/${pk}/`, {...data}).then(r => r.data)
+        return instance.patch(`companies/${pk}`, {...data}).then(r => r.data)
     },
     delete(pk: number) {
-        return instance.delete(`companies/${pk}/`).then(r => r.data)
+        return instance.delete(`companies/${pk}`).then(r => r.data)
     },
 }
--- frontend/src/redux/company-reducer.ts
+++ frontend/src/redux/company-reducer.ts
@@ -1,6 +1,7 @@
 import {
     addSuccessMessage,
-    actions as appActions,
+    actions as actionsApp, commonAsyncHandler,
+    TActions as TActionsApp,
 } from "./app-reducer"
 import {companyAPI} from "../api/company-api"
 import {TBaseThunk, TInferActions} from "./redux-store";
@@ -43,31 +44,32 @@ type TActions = TInferActions<typeof actions>
 // ACTIONS
 export const actions = {
     companyListAction: (results: Array<TCompany>) => ({ type: SET_COMPANY_LIST, payload: {results} as const}),
-    // setFormErrorsAction: (formErrors) => ({ type: SET_FORM_ERROR_MESSAGES, payload: {formErrors}})
+    // todo: any refactor
+    setFormErrorsAction: (formErrors: any) => ({ type: SET_FORM_ERROR_MESSAGES, payload: {formErrors}})
 }
 
 // THUNKS
-type TThunk = TBaseThunk<TActions>
+type TThunk = TBaseThunk<TActions | TActionsApp>
 
 export const getCompanyList = (): TThunk => async (dispatch) => {
-    // dispatch(appActions.setAppLoading(true))
-    const data = await companyAPI.company_list()
-    dispatch(actions.companyListAction([data]))
-    // dispatch(appActions.setAppLoading(false))
+    await commonAsyncHandler(async () => {
+        const data = await companyAPI.company_list()
+        dispatch(actions.companyListAction(data))
+    }, dispatch)
 }
 
 export const createCompany = (data: TCompany): TThunk => async (dispatch) => {
     try{
         await companyAPI.create(data)
         dispatch(addSuccessMessage('Company has been created'))
-        // dispatch(appActions.redirectToAction('/companies'))
-        // dispatch(actions.setFormErrorsAction({}))
+        dispatch(actionsApp.redirectToAction('/companies'))
+        dispatch(actions.setFormErrorsAction({}))
     } catch (e) {
         const response = e.response
         const errors = response.data
         // dispatch(actions.setFormErrorsAction(errors))
     } finally {
-        // dispatch(appActions.setAppLoading(false))
+        dispatch(actionsApp.setAppLoading(false))
     }
 }
 
