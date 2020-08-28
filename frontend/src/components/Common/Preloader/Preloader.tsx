d720aa5d8469cf0a71f7225471cb73066bcae142
Dy0mar <kurtudins@gmail.com>
2020-08-28 19:15:32 +0300

    Preloader.jsx to TSX

--- frontend/src/components/Common/Preloader/Preloader.tsx
+++ frontend/src/components/Common/Preloader/Preloader.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import css from "./Preloader.module.css";
-import Spin from "antd/es/spin";
+import css from "./Preloader.module.css"
+import Spin from "antd/es/spin"
 
 
-const Preloader = (props) => {
+const Preloader: React.FC = () => {
     return (
         <div className={css.outer}>
             <Spin size="large" />
         </div>
     )
-};
+}
 
 export default Preloader
