ed8c8f4e7a4694d5b412d9fc69a5add27ad29e56
Dy0mar <kurtudins@gmail.com>
2020-08-28 19:12:05 +0300

    Breadcrumb.jsx to TSX

---
+++ frontend/src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Breadcrumb} from "antd"
+import {Link, withRouter} from "react-router-dom"
+import {compose} from "redux"
+import {RouteComponentProps} from "react-router"
+
+const BreadcrumbComponent: React.FC<RouteComponentProps> = (props) => {
+
+    const { location } = props
+    const pathSnippets = location.pathname.split('/').filter(i => i)
+
+    const extraBreadcrumbItems = pathSnippets.map((item, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
+        return (
+            <Breadcrumb.Item key={url}>
+                <Link to={url}>{item}</Link>
+            </Breadcrumb.Item>
+        )
+    })
+    const breadcrumbItems = [
+        <Breadcrumb.Item key="home">
+            <Link to="/">Home</Link>
+        </Breadcrumb.Item>,
+    ].concat(extraBreadcrumbItems)
+
+    return (
+        <Breadcrumb style={{ margin: '16px 0' }}>
+            {breadcrumbItems}
+        </Breadcrumb>
+    )
+}
+
+
+export default compose<React.ComponentType>(
+    withRouter,
+)(BreadcrumbComponent)
