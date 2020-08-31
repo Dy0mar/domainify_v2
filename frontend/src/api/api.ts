49193177a9a5f9ca059588e05f144826e95d23f8
Dy0mar <kurtudins@gmail.com>
2020-08-31 11:12:07 +0300

    api.js to TS

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,6 @@
-import * as axios from "axios"
+import axios from "axios"
 import {url} from './privacy'
+import {TProfile} from "../types/g-types"
 
 
 const instance = axios.create({
@@ -15,61 +16,61 @@ instance.interceptors.request.use(
         const token = localStorage.getItem("token")
         if (token)
             config.headers.Authorization = `Bearer ${token}`
-        return config;
+        return config
     },
     function (error) {
-        return Promise.reject (error);
+        return Promise.reject (error)
     }
 )
 
 
 export const usersAPI = {
-    register(username, email, password, profile) {
+    register(username: string, email: string, password: string, profile: TProfile) {
         return instance.post('users/', {username, email, password, profile})
     },
-    get_user_info(pk){
+    get_user_info(pk: number){
         return instance.get(`users/${pk}/`)
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`users/${pk}/`, {...data})
     },
-    update_profile(pk, data){
+    update_profile(pk: number, data: any){
         return instance.put(`users/${pk}/`, {...data})
     },
-    get_user_list(page){
+    get_user_list(page: number){
         return instance.get(`users/?page=${page}`)
     },
     manager_list(){
         return instance.get('users/manager_list/')
     },
-    check_notification_method(m){
+    check_notification_method(m: 'jabber' | 'email'){
         return instance.get('users/check-notification-method/?method='+m)
     }
 
 }
 
 export const domainsAPI = {
-    create(data){
+    create(data: any){
         return instance.post('domains/', {...data})
     },
-    delete(pk){
+    delete(pk: number){
         return instance.delete(`domains/${pk}/`)
     },
-    domain_detail(pk){
+    domain_detail(pk: number){
         return instance.get(`domains/${pk}/`)
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`domains/${pk}/`, {...data})
     },
-    domain_list(page, filters=[]){
-        let url = `domains/?page=${page}`;
-        let part_url = convertObjectToUrl(filters);
+    domain_list(page: number, filters=[]){
+        let url = `domains/?page=${page}`
+        let part_url = convertObjectToUrl(filters)
 
         if (part_url !== '')
-            url += part_url;
+            url += part_url
         return instance.get(url).then(r => r.data)
     },
-    autocomplete_domain_list(term){
+    autocomplete_domain_list(term: string){
         return instance.get(`domains/autocomplete-domain-list/?term=${term}`)
     },
     status_list(){
@@ -88,7 +89,7 @@ export const authAPI = {
     me() {
         return instance.get('auth/user/').then(r => r.data)
     },
-    login(username, password) {
+    login(username: string, password: string) {
         return instance.post('auth/login/', {username, password})
     },
     logout() {
@@ -100,36 +101,36 @@ export const companyAPI = {
     company_list(){
         return instance.get('companies/').then(r => r.data)
     },
-    create(data){
+    create(data: any){
         return instance.post('companies/', {...data})
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`companies/${pk}/`, {...data})
     },
-    delete(pk){
+    delete(pk: number){
         return instance.delete(`companies/${pk}/`)
     },
 }
 
 export const taskAPI = {
-    task_list(page, filters=[]){
-        let url = `tasks/?page=${page}`;
-        let part_url = convertObjectToUrl(filters);
+    task_list(page: number, filters=[]){
+        let url = `tasks/?page=${page}`
+        let part_url = convertObjectToUrl(filters)
 
         if (part_url !== '')
-            url += part_url;
+            url += part_url
         return instance.get(url)
     },
-    create(data){
+    create(data: any){
         return instance.post('tasks/', {...data})
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`tasks/${pk}/`, {...data})
     },
-    detail(pk){
+    detail(pk: number){
         return instance.get(`tasks/${pk}/`)
     },
-    delete(pk){
+    delete(pk: number){
         return instance.delete(`tasks/${pk}/`)
     },
 }
@@ -138,16 +139,16 @@ export const statusAPI = {
     status_list(){
         return instance.get('statuses/').then(r => r.data)
     },
-    create(data){
+    create(data: any){
         return instance.post('statuses/', {...data})
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`statuses/${pk}/`, {...data})
     },
-    detail(pk){
+    detail(pk: number){
         return instance.get(`statuses/${pk}/`)
     },
-    delete(pk){
+    delete(pk: number){
         return instance.delete(`statuses/${pk}/`)
     },
 }
@@ -156,33 +157,37 @@ export const codesAPI = {
     codes_list(){
         return instance.get('codes/').then(r => r.data)
     },
-    create(data){
+    create(data: any){
         return instance.post('codes/', {...data})
     },
-    patch_field(pk, data){
+    patch_field(pk: number, data: any){
         return instance.patch(`codes/${pk}/`, {...data})
     },
-    detail(pk){
+    detail(pk: number){
         return instance.get(`codes/${pk}/`)
     },
-    delete(pk){
+    delete(pk: number){
         return instance.delete(`codes/${pk}/`)
     },
 }
 
 
 
-function convertObjectToUrl(objList) {
+function convertObjectToUrl(objList: any) {
     // objList = [{param: ['value1', 'value2', ...]}, {...}]
+    // to -> api/domains/?page=1&manager=9&manager=7
     if (objList.length === 0 && Object.entries(objList[0]).length === 0)
         return ''
 
     let url = ''
-    objList.forEach(paramObj => {
-        for (let [param, values] of Object.entries(paramObj)){
-            if (values.length !== 0){
-                let separator = '&' + param + '=';
-                url += separator + values.join(separator);
+    objList.forEach((paramObj: any) => {
+        let ss = Object.entries(paramObj)
+        for (let [paramName, paramValues] of ss) {
+            // @ts-ignore
+            if (paramValues.length !== 0) {
+                let separator = '&' + paramName + '='
+                // @ts-ignore
+                url += separator + values.join(separator)
             }
         }
     })
--- frontend/src/types/g-types.ts
+++ frontend/src/types/g-types.ts
@@ -1,5 +1,14 @@
 export type TMessageTypes = 'success' | 'error'
 
+export type TProfile = {
+    jabber_nick: string
+}
+
+export type TSettings = {
+    jabber: boolean
+    email: boolean
+}
+
 export type TMessage = {
     id: number,
     type: TMessageTypes,
