207038db2a3095a9667a7356c0f5cc6837ba57ce
Dy0mar <kurtudins@gmail.com>
2020-09-17 11:46:00 +0300

    App to tsx

--- frontend/src/selectors/app-selector.ts
+++ frontend/src/selectors/app-selector.ts
@@ -9,3 +9,5 @@ export const getIsLoadingS = (state: TAppState): boolean => state.app.isLoading
 export const getUrlOr404S = (url: string): string => {
     return  url ? '/' + url.split('api/')[1] : 'page404'
 }
+
+export const isInitializedS = (state: TAppState): boolean => state.app.initialized
